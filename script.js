7493b18a4d7f17654cbd96f558c857ae6bc622da
Mengya Zeng <echomaria98123@gmail.com>
2022-11-05 16:19:23 -0700

    add web element

---
+++ script.js
@@ -0,0 +1,25 @@
+//alert("hello, world")
+
+//console.log('hello, Echo')
+
+let name = 'Echo'
+
+function say_hello(name){
+  console.log('hello,'+ name + '!')
+}
+
+// say_hello(name);
+
+// let car_color = 
+function alert_car(car_color){
+  alert("Car color is " + car_color)
+}
+
+// alert_car("blue")
+
+function increment() {
+	score_element = document.getElementById("score");
+	score_element.innerHTML = parseInt(score_element.innerHTML) + 1;
+}
+
+
